371471309d67fae72798ba078f5e08a4fd70b00e
ac117108 <ac117108@8CG8163L28>
2022-03-15 10:40:33 +1300

    Add project files.

---
+++ Exercise5a/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Console.WriteLine("This program will display an employee's total weekly pay.\n\n");
+
+double wage, regularHours, overtimeHours;
+Console.Write("Enter hourly wage: ");
+
+wage = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Enter total number of regular hours: ");
+
+regularHours = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Enter overtime hours: ");
+
+overtimeHours = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("The total weekly pay = $" + ((wage * regularHours) + (1.5 * wage * overtimeHours)));
